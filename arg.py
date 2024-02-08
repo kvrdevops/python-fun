9e4e9c30b6de144bc8e2d9fe80fd5c433426a18c
kvrdevops <150337637+kvrdevops@users.noreply.github.com>
2024-02-09 00:14:10 +0530

    Update arg.py

--- arg.py
+++ arg.py
@@ -6,9 +6,9 @@ def sub (num1, num2):
     s = num1-num2
     return s
 num1 = float(sys.argv[1])
-operation = float(sys.argv[2])
-num2 = sys.argv[3]
+operation = (sys.argv[2])
+num2 = float(sys.argv[3])
 
-if operation == "add"
+if operation == "add":
   output=add(num1,num2)
   print(output)
